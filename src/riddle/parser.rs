368721301c2d17b9c88d5b904bc968c2adb4f2b9
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-01-11 22:27:52 +0100

    Add Token type to parser and implement peek, next, and expect methods

--- src/riddle/parser.rs
+++ src/riddle/parser.rs
@@ -1,4 +1,4 @@
-use crate::riddle::lexer::Lexer;
+use crate::riddle::lexer::{Lexer, Token};
 use std::iter::Peekable;
 
 pub struct Parser<'a> {
@@ -9,6 +9,22 @@ impl<'a> Parser<'a> {
     pub fn new(lexer: Lexer<'a>) -> Self {
         Parser {
             lexer: lexer.peekable(),
+        }
+    }
+
+    fn peek(&mut self) -> Option<&Token> {
+        self.lexer.peek()
+    }
+
+    fn next(&mut self) -> Option<Token> {
+        self.lexer.next()
+    }
+
+    fn expect(&mut self, expected: Token) -> Result<Token, String> {
+        match self.next() {
+            Some(token) if token == expected => Ok(token),
+            Some(token) => Err(format!("Expected {:?}, found {:?}", expected, token)),
+            None => Err(format!("Expected {:?}, found end of input", expected)),
         }
     }
 }
