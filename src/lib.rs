b66ac5b3c86c5727d0fed97ea69c6fcb9cdd17fe
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-01-12 12:08:57 +0100

    Add modules for AC and Lin solvers with initial struct definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 pub mod utils;
-pub use utils::rational::Rational;
 pub use utils::inf_rational::InfRational;
-pub use utils::lit::Lit;
 pub use utils::lin::Lin;
+pub use utils::lit::Lit;
+pub use utils::rational::Rational;
+
+pub mod ac;
+pub use ac::ac::Solver as AcSolver;
+
+pub mod lin;
+pub use lin::lin::Solver as LinSolver;
 
 pub mod riddle;
