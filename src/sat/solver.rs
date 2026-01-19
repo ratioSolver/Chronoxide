102fa4ca7a852ed6d1f30bf0006c3dad46f757a1
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-01-19 12:52:13 +0100

    Refactor variable representation in Solver: replace tuple with Var struct for improved clarity and organization

--- src/sat/solver.rs
+++ src/sat/solver.rs
@@ -1,11 +1,28 @@
 use crate::{Lit, utils::lit::LBool};
-use std::collections::{HashMap, HashSet, VecDeque};
-
+use std::collections::{HashMap, VecDeque};
 type Callback = Box<dyn Fn(&Solver, usize)>;
+
+struct Var {
+    value: LBool,            // current value
+    _reason: Option<usize>,  // clause that implied the value
+    pos_clauses: Vec<usize>, // clauses where the variable appears positively
+    neg_clauses: Vec<usize>, // clauses where the variable appears negatively
+}
+
+impl Var {
+    pub fn new() -> Self {
+        Var {
+            value: LBool::Undef,
+            _reason: None,
+            pos_clauses: Vec::new(),
+            neg_clauses: Vec::new(),
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct Solver {
-    vars: Vec<(LBool, Vec<usize>, Vec<usize>)>, // (value, pos clauses, neg clauses)
+    vars: Vec<Var>,
     clauses: Vec<Vec<Lit>>,
     prop_q: VecDeque<(usize, Option<usize>)>, // (var, reason clause)
     listeners: HashMap<usize, Vec<Callback>>,
@@ -18,21 +35,21 @@ impl Solver {
 
     pub fn add_var(&mut self) -> usize {
         let var_id = self.vars.len();
-        self.vars.push((LBool::Undef, Vec::new(), Vec::new()));
+        self.vars.push(Var::new());
         var_id
     }
 
     pub fn value(&self, var: usize) -> LBool {
-        self.vars[var].0.clone()
+        self.vars[var].value.clone()
     }
 
     pub fn add_clause(&mut self, lits: &[Lit]) -> bool {
         let clause_id = self.clauses.len();
         for lit in lits {
             if lit.is_positive() {
-                self.vars[lit.var()].1.push(clause_id);
+                self.vars[lit.var()].pos_clauses.push(clause_id);
             } else {
-                self.vars[lit.var()].2.push(clause_id);
+                self.vars[lit.var()].neg_clauses.push(clause_id);
             }
         }
         self.clauses.push(lits.to_vec());
@@ -43,9 +60,9 @@ impl Solver {
         self.enqueue(lit, None);
         while let Some((var, reason)) = self.prop_q.pop_front() {
             let clauses = if self.value(var) == LBool::True {
-                self.vars[var].1.clone()
+                self.vars[var].pos_clauses.clone()
             } else {
-                self.vars[var].2.clone()
+                self.vars[var].neg_clauses.clone()
             };
             for clause_id in clauses {
                 if reason != Some(clause_id) && !self.propagate(clause_id) {
@@ -56,37 +73,9 @@ impl Solver {
         true
     }
 
-    pub fn retract(&mut self, var: usize) {
-        self.vars[var].0 = LBool::Undef;
-        let mut visited: HashSet<usize> = HashSet::new();
-        visited.insert(var);
-        let mut to_retract: VecDeque<usize> = VecDeque::new();
-        to_retract.push_back(var);
-        while let Some(v) = to_retract.pop_front() {
-            let n_pos = self.vars[v].1.len();
-            let n_neg = self.vars[v].2.len();
-            for i in 0..n_pos + n_neg {
-                let clause_id = if i < n_pos {
-                    self.vars[v].1[i]
-                } else {
-                    self.vars[v].2[i - n_pos]
-                };
-
-                for &lit in &self.clauses[clause_id] {
-                    let lit_var = lit.var();
-                    if !visited.contains(&lit_var) && self.value(lit_var) != LBool::Undef {
-                        visited.insert(lit_var);
-                        to_retract.push_back(lit_var);
-                        self.vars[lit_var].0 = LBool::Undef;
-                    }
-                }
-            }
-        }
-    }
-
     fn propagate(&mut self, clause_id: usize) -> bool {
         let mut num_undef = 0;
-        let mut last_undef: Option<Lit> = None;
+        let mut last_undef = None;
         for &lit in &self.clauses[clause_id] {
             match self.value(lit.var()) {
                 LBool::True => return true,
@@ -110,7 +99,7 @@ impl Solver {
     fn enqueue(&mut self, lit: &Lit, reason: Option<usize>) -> bool {
         match self.value(lit.var()) {
             LBool::Undef => {
-                self.vars[lit.var()].0 = if lit.is_positive() {
+                self.vars[lit.var()].value = if lit.is_positive() {
                     LBool::True
                 } else {
                     LBool::False
