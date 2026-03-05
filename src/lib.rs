7fa39e6c3296ca9c0c8a1b902f0a8a920c7c5441
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-03-05 11:45:11 +0100

    Refactor Solver structure: replace Scope and Env with Core for improved encapsulation and method access

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use linspire::{
     rational::rat,
 };
 use riddle::{
-    env::{BoolType, CommonEnv, CommonScope, Core, Env, Field, IntType, Method, RealType, Scope, StringType, Type, Var},
+    env::{BoolType, CommonCore, CommonEnv, CommonScope, Core, Env, Field, IntType, Method, RealType, Scope, StringType, Type, Var},
     language::{EnumDef, PredicateDef},
 };
 use std::{
@@ -18,8 +18,7 @@ use crate::objects::{BoolVar, IntVar, RealVar, StringVar};
 mod objects;
 
 pub struct Solver {
-    scope: Rc<CommonScope>,
-    env: Rc<CommonEnv>,
+    core: Rc<CommonCore>,
     sat: RefCell<consensus::Engine>,
     ac: RefCell<dynamic_ac::Engine>,
     lin: RefCell<linspire::Engine>,
@@ -27,17 +26,15 @@ pub struct Solver {
 
 impl Solver {
     pub fn new() -> Rc<Self> {
-        let core = Rc::new_cyclic(|core| Self {
-            scope: {
+        Rc::new_cyclic(|core| Self {
+            core: {
                 let core: Weak<Solver> = core.clone();
-                Rc::new(CommonScope::new(core.clone(), None))
+                CommonCore::new(core)
             },
-            env: Rc::new(CommonEnv::new(None)),
             sat: RefCell::new(consensus::Engine::new()),
             ac: RefCell::new(dynamic_ac::Engine::new()),
             lin: RefCell::new(linspire::Engine::new()),
-        });
-        core
+        })
     }
 
     pub fn bool_val(&self, obj: &BoolVar) -> LBool {
@@ -62,19 +59,19 @@ impl Scope for Solver {
     }
 
     fn get_field(&self, name: &str) -> Option<Rc<Field>> {
-        self.scope.get_field(name)
+        self.core.get_field(name)
     }
     fn get_method(&self, name: &str, types: &[Rc<dyn Type>]) -> Option<Rc<Method>> {
-        self.scope.get_method(name, types)
+        self.core.get_method(name, types)
     }
     fn get_class(&self, name: &str) -> Option<Rc<dyn Type>> {
-        self.scope.get_class(name)
+        self.core.get_class(name)
     }
     fn get_enum(&self, name: &str) -> Option<Rc<EnumDef>> {
-        self.scope.get_enum(name)
+        self.core.get_enum(name)
     }
     fn get_predicate(&self, name: &str) -> Option<Rc<PredicateDef>> {
-        self.scope.get_predicate(name)
+        self.core.get_predicate(name)
     }
 }
 
@@ -83,48 +80,37 @@ impl Env for Solver {
         None
     }
     fn get(&self, name: &str) -> Option<Rc<dyn Var>> {
-        self.env.get(name)
+        self.core.get(name)
     }
     fn set(&self, name: String, value: Rc<dyn Var>) {
-        self.env.set(name, value)
+        self.core.set(name, value)
     }
 }
 
 impl Core for Solver {
     fn new_bool(&self, value: bool) -> Rc<dyn Var> {
-        let bool_type = self.scope.get_class("bool").expect("Bool type not found").as_any().downcast::<BoolType>().expect("Bool type is not BoolType");
-        Rc::new(BoolVar::new(bool_type, if value { TRUE_LIT } else { FALSE_LIT }))
+        Rc::new(BoolVar::new(self.core.bool_type(), if value { TRUE_LIT } else { FALSE_LIT }))
     }
     fn new_bool_var(&self) -> Rc<dyn Var> {
-        let var = self.sat.borrow_mut().add_var();
-        let bool_type = self.scope.get_class("bool").expect("Bool type not found").as_any().downcast::<BoolType>().expect("Bool type is not BoolType");
-        Rc::new(BoolVar::new(bool_type, pos(var)))
+        Rc::new(BoolVar::new(self.core.bool_type(), pos(self.sat.borrow_mut().add_var())))
     }
     fn new_int(&self, value: i64) -> Rc<dyn Var> {
-        let int_type = self.scope.get_class("Int").expect("Int type not found").as_any().downcast::<IntType>().expect("Int type is not IntType");
-        Rc::new(IntVar::new(int_type, c(value)))
+        Rc::new(IntVar::new(self.core.int_type(), c(value)))
     }
     fn new_int_var(&self) -> Rc<dyn Var> {
-        let var = self.lin.borrow_mut().add_var();
-        let int_type = self.scope.get_class("Int").expect("Int type not found").as_any().downcast::<IntType>().expect("Int type is not IntType");
-        Rc::new(IntVar::new(int_type, v(var)))
+        Rc::new(IntVar::new(self.core.int_type(), v(self.lin.borrow_mut().add_var())))
     }
     fn new_real(&self, num: i64, den: i64) -> Rc<dyn Var> {
-        let real_type = self.scope.get_class("real").expect("Real type not found").as_any().downcast::<RealType>().expect("Real type is not RealType");
-        Rc::new(RealVar::new(real_type, Lin::new_const(rat(num, den))))
+        Rc::new(RealVar::new(self.core.real_type(), Lin::new_const(rat(num, den))))
     }
     fn new_real_var(&self) -> Rc<dyn Var> {
-        let var = self.lin.borrow_mut().add_var();
-        let real_type = self.scope.get_class("real").expect("Real type not found").as_any().downcast::<RealType>().expect("Real type is not RealType");
-        Rc::new(RealVar::new(real_type, v(var)))
+        Rc::new(RealVar::new(self.core.real_type(), v(self.lin.borrow_mut().add_var())))
     }
     fn new_string(&self, value: &str) -> Rc<dyn Var> {
-        let string_type = self.scope.get_class("string").expect("String type not found").as_any().downcast::<StringType>().expect("String type is not StringType");
-        Rc::new(StringVar::new(string_type, value.to_string()))
+        Rc::new(StringVar::new(self.core.string_type(), value.to_string()))
     }
     fn new_string_var(&self) -> Rc<dyn Var> {
-        let string_type = self.scope.get_class("string").expect("String type not found").as_any().downcast::<StringType>().expect("String type is not StringType");
-        Rc::new(StringVar::new(string_type, String::new()))
+        Rc::new(StringVar::new(self.core.string_type(), String::new()))
     }
 
     fn sum(&self, sum: &[Rc<dyn Var>]) -> Rc<dyn Var> {
