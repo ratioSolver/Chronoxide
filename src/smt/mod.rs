e8adab677a99553305b9e342f7a497affffcdc1e
riccardodebenedictis <riccardo.debenedictis@cnr.it>
2026-08-04 10:19:17 +0200

    feat: add Linear Real Arithmetic (LRA) module and integrate into SMT structure

--- src/smt/mod.rs
+++ src/smt/mod.rs
@@ -1,12 +1,14 @@
 pub mod ast;
 mod lin;
 mod lit;
+mod lra;
 mod rational;
 
 use crate::smt::{
     ast::{ArithExpr, BoolExpr, EnumExpr, Expr},
     lin::Lin,
     lit::Lit,
+    lra::LRA,
     rational::{InfRational, Rational},
 };
 use std::{
@@ -17,24 +19,18 @@ use std::{
 use tracing::trace;
 
 pub struct SMT {
-    sat_to_ast: Vec<Option<BoolExpr>>,                            // Map from SAT variable index to its corresponding AST expression
-    ast_to_sat: HashMap<BoolExpr, usize>,                         // Map from AST expression to its corresponding SAT variable index
-    bools: Vec<Option<bool>>,                                     // Current assignment of each boolean variable (true, false, or unassigned)
-    clauses: Vec<Clause>,                                         // List of clauses in the solver
-    watches: Vec<Vec<usize>>,                                     // Watch lists for each literal (positive and negative)
-    reason: Vec<Option<usize>>,                                   // Reason for each variable's assignment
-    prop_q: VecDeque<Lit>,                                        // Queue of literals to propagate
-    trail: Vec<Lit>,                                              // Trail of assigned literals for backtracking
-    trail_lim: Vec<usize>,                                        // Indices in the trail where decisions were made
-    level: Vec<Option<usize>>,                                    // Decision level for each variable
-    ints: Vec<bool>,                                              // Distinguish between integer and real variables
-    reals: Vec<Rational>,                                         // Current assignments of real variables
-    lbs: Vec<Rational>,                                           // Current assignments of lower bounds
-    ubs: Vec<Rational>,                                           // Current assignments of upper bounds
-    lin_to_slack: HashMap<BTreeMap<usize, rug::Rational>, usize>, // Mapping from linear constraints to their corresponding slack variable
-    tableau: BTreeMap<usize, BTreeMap<usize, rug::Rational>>,     // Tableau for linear constraints
-    bound_trail: Vec<BoolExpr>,                                   // Trail of bound updates for backtracking
-    enums: Vec<HashMap<i32, usize>>,                              // Enum variables
+    sat_to_ast: Vec<Option<BoolExpr>>,    // Map from SAT variable index to its corresponding AST expression
+    ast_to_sat: HashMap<BoolExpr, usize>, // Map from AST expression to its corresponding SAT variable index
+    bools: Vec<Option<bool>>,             // Current assignment of each boolean variable (true, false, or unassigned)
+    clauses: Vec<Clause>,                 // List of clauses in the solver
+    watches: Vec<Vec<usize>>,             // Watch lists for each literal (positive and negative)
+    reason: Vec<Option<usize>>,           // Reason for each variable's assignment
+    prop_q: VecDeque<Lit>,                // Queue of literals to propagate
+    trail: Vec<Lit>,                      // Trail of assigned literals for backtracking
+    trail_lim: Vec<usize>,                // Indices in the trail where decisions were made
+    level: Vec<Option<usize>>,            // Decision level for each variable
+    lra: LRA,                             // Linear Real Arithmetic solver
+    enums: Vec<HashMap<i32, usize>>,      // Enum variables
 }
 
 impl Default for SMT {
@@ -56,13 +52,7 @@ impl SMT {
             trail: Vec::new(),
             trail_lim: Vec::new(),
             level: Vec::new(),
-            ints: Vec::new(),
-            reals: Vec::new(),
-            lbs: Vec::new(),
-            ubs: Vec::new(),
-            lin_to_slack: HashMap::new(),
-            tableau: BTreeMap::new(),
-            bound_trail: Vec::new(),
+            lra: LRA::new(),
             enums: Vec::new(),
         }
     }
@@ -264,48 +254,6 @@ impl SMT {
         }
     }
 
-    pub fn eval_arith(&self, expr: &ArithExpr) -> (Rational, Rational, Rational) {
-        match expr {
-            ArithExpr::Const(r) => (r.clone(), r.clone(), r.clone()),
-            ArithExpr::IntVar(v) => (self.lbs[*v].clone(), self.reals[*v].clone(), self.ubs[*v].clone()),
-            ArithExpr::RealVar(v) => (self.lbs[*v].clone(), self.reals[*v].clone(), self.ubs[*v].clone()),
-            ArithExpr::Add(add) => {
-                let mut lb = Rational::Finite(rug::Rational::from(0));
-                let mut res = Rational::Finite(rug::Rational::from(0));
-                let mut ub = Rational::Finite(rug::Rational::from(0));
-                for sub_expr in add {
-                    let (sub_lb, sub_res, sub_ub) = self.eval_arith(sub_expr);
-                    lb += sub_lb;
-                    res += sub_res;
-                    ub += sub_ub;
-                }
-                (lb, res, ub)
-            }
-            ArithExpr::Sub(e1, e2) => {
-                let (lb1, res1, ub1) = self.eval_arith(e1);
-                let (lb2, res2, ub2) = self.eval_arith(e2);
-                (lb1 - ub2, res1 - res2, ub1 - lb2)
-            }
-            ArithExpr::Mul(mul) => {
-                let mut lb = Rational::Finite(rug::Rational::from(1));
-                let mut res = Rational::Finite(rug::Rational::from(1));
-                let mut ub = Rational::Finite(rug::Rational::from(1));
-                for sub_expr in mul {
-                    let (sub_lb, sub_res, sub_ub) = self.eval_arith(sub_expr);
-                    lb *= sub_lb;
-                    res *= sub_res;
-                    ub *= sub_ub;
-                }
-                (lb, res, ub)
-            }
-            ArithExpr::Div(e1, e2) => {
-                let (lb1, res1, ub1) = self.eval_arith(e1);
-                let (lb2, res2, ub2) = self.eval_arith(e2);
-                (lb1 / ub2, res1 / res2, ub1 / lb2)
-            }
-        }
-    }
-
     fn mk_bool(&mut self) -> usize {
         let idx = self.bools.len();
         self.bools.push(None);
@@ -314,24 +262,6 @@ impl SMT {
         self.reason.push(None);
         self.level.push(None);
         idx
-    }
-
-    fn mk_int(&mut self) -> usize {
-        let var_index = self.ints.len();
-        self.ints.push(true);
-        self.reals.push(Rational::Finite(rug::Rational::from(0))); // Initialize with 0
-        self.lbs.push(Rational::NegativeInf); // Initialize lower bound to -inf
-        self.ubs.push(Rational::PositiveInf); // Initialize upper bound to +inf
-        var_index
-    }
-
-    fn mk_real(&mut self) -> usize {
-        let var_index = self.reals.len();
-        self.ints.push(false);
-        self.reals.push(Rational::Finite(rug::Rational::from(0))); // Initialize with 0
-        self.lbs.push(Rational::NegativeInf); // Initialize lower bound to -inf
-        self.ubs.push(Rational::PositiveInf); // Initialize upper bound to +inf
-        var_index
     }
 
     fn get_proxy(&mut self, expr: &BoolExpr) -> Option<usize> {
@@ -529,34 +459,6 @@ impl SMT {
         }
     }
 
-    fn mk_le(&mut self, e1: &ArithExpr, e2: &ArithExpr, strict: bool) -> BoolExpr {
-        let (vars, const_term) = self.diff(e1, e2);
-
-        match vars.len() {
-            0 => BoolExpr::from(if strict { const_term.is_negative() } else { const_term.is_negative() || const_term.is_zero() }),
-            1 => {
-                let (&var, coeff) = vars.iter().next().unwrap();
-
-                let eps_val = if strict { rug::Rational::from(-1) } else { rug::Rational::from(0) };
-                let bound = InfRational::new(-const_term.clone() / coeff, eps_val / coeff);
-                BoolExpr::Var(self.get_or_create_proxy(if coeff.is_positive() { BoolExpr::Ub(var, bound) } else { BoolExpr::Lb(var, bound) }))
-            }
-            _ => {
-                let slack = if let Some(&slack) = self.lin_to_slack.get(&vars) {
-                    slack
-                } else {
-                    let slack = self.mk_real();
-                    self.tableau.insert(slack, vars.clone());
-                    self.lin_to_slack.insert(vars, slack);
-                    slack
-                };
-
-                let eps_val = if strict { rug::Rational::from(-1) } else { rug::Rational::from(0) };
-                BoolExpr::Var(self.get_or_create_proxy(BoolExpr::Ub(slack, InfRational::new(-const_term, eps_val))))
-            }
-        }
-    }
-
     fn mk_bool_eq(&mut self, e1: &BoolExpr, e2: &BoolExpr) -> BoolExpr {
         let e1 = self.mk_expr(e1);
         let e2 = self.mk_expr(e2);
@@ -596,80 +498,6 @@ impl SMT {
 
             BoolExpr::Var(proxy)
         }
-    }
-
-    fn mk_arith_eq(&mut self, e1: &ArithExpr, e2: &ArithExpr) -> BoolExpr {
-        if e1 == e2 {
-            return BoolExpr::True;
-        }
-        let (vars, const_term) = self.diff(e1, e2);
-
-        match vars.len() {
-            0 => BoolExpr::from(const_term.is_zero()),
-            1 => {
-                let (&var, coeff) = vars.iter().next().unwrap();
-                let bound = InfRational::new(-const_term.clone() / coeff, rug::Rational::from(0));
-                BoolExpr::Var(self.get_or_create_proxy(BoolExpr::ArithEq(var, bound)))
-            }
-            _ => {
-                let slack = if let Some(&slack) = self.lin_to_slack.get(&vars) {
-                    slack
-                } else {
-                    let slack = self.mk_real();
-                    self.tableau.insert(slack, vars.clone());
-                    self.lin_to_slack.insert(vars, slack);
-                    slack
-                };
-
-                BoolExpr::Var(self.get_or_create_proxy(BoolExpr::ArithEq(slack, InfRational::new(-const_term, rug::Rational::from(0)))))
-            }
-        }
-    }
-
-    fn mk_ge(&mut self, e1: &ArithExpr, e2: &ArithExpr, strict: bool) -> BoolExpr {
-        let (vars, const_term) = self.diff(e1, e2);
-
-        match vars.len() {
-            0 => BoolExpr::from(if strict { const_term.is_positive() } else { const_term.is_positive() || const_term.is_zero() }),
-            1 => {
-                let (&var, coeff) = vars.iter().next().unwrap();
-
-                let eps_val = if strict { rug::Rational::from(1) } else { rug::Rational::from(0) };
-                let bound = InfRational::new(-const_term.clone() / coeff, eps_val / coeff);
-                BoolExpr::Var(self.get_or_create_proxy(if coeff.is_positive() { BoolExpr::Lb(var, bound) } else { BoolExpr::Ub(var, bound) }))
-            }
-            _ => {
-                let slack = if let Some(&slack) = self.lin_to_slack.get(&vars) {
-                    slack
-                } else {
-                    let slack = self.mk_real();
-                    self.tableau.insert(slack, vars.clone());
-                    self.lin_to_slack.insert(vars, slack);
-                    slack
-                };
-
-                let eps_val = if strict { rug::Rational::from(1) } else { rug::Rational::from(0) };
-                BoolExpr::Var(self.get_or_create_proxy(BoolExpr::Lb(slack, InfRational::new(-const_term, eps_val))))
-            }
-        }
-    }
-
-    fn diff(&self, e1: &ArithExpr, e2: &ArithExpr) -> (BTreeMap<usize, rug::Rational>, rug::Rational) {
-        let diff = Lin::from(e1) - Lin::from(e2);
-        let mut vars = BTreeMap::new();
-
-        for (var, coeff) in diff.vars {
-            if let Some(basic) = self.tableau.get(&var) {
-                for (&sub_var, sub_coeff) in basic {
-                    *vars.entry(sub_var).or_insert_with(|| rug::Rational::from(0)) += coeff.clone() * sub_coeff;
-                }
-            } else {
-                *vars.entry(var).or_insert_with(|| rug::Rational::from(0)) += coeff;
-            }
-        }
-
-        vars.retain(|_, c| *c != 0);
-        (vars, diff.const_term)
     }
 
     pub fn propagate(&mut self) -> Result<(), BoolExpr> {
