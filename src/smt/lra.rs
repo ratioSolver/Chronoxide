072b9093644226c1cea065be744b7c218da68864
riccardodebenedictis <riccardo.debenedictis@cnr.it>
2026-08-05 12:57:03 +0200

    feat: update LraTheory to include guard literals in bounds and add check method for variable bounds

--- src/smt/lra.rs
+++ src/smt/lra.rs
@@ -11,8 +11,8 @@ use std::{
 pub(super) struct LraTheory {
     ints: Vec<bool>,                                                       // true = integer variable, false = real variable
     reals: Vec<InfRational>,                                               // Current assignments
-    lbs: Vec<InfRational>,                                                 // Current lower bounds
-    ubs: Vec<InfRational>,                                                 // Current upper bounds
+    lbs: Vec<(Option<Lit>, InfRational)>,                                  // Current lower bounds
+    ubs: Vec<(Option<Lit>, InfRational)>,                                  // Current upper bounds
     pub(super) lin_to_slack: HashMap<BTreeMap<usize, RugRational>, usize>, // Mapping from linear constraints to their slack variable
     pub(super) tableau: BTreeMap<usize, BTreeMap<usize, RugRational>>,     // Tableau: basic variable -> linear expression over non-basic variables
     t_watches: Vec<HashSet<usize>>,                                        // For each variable, the set of tableau rows containing it
@@ -48,8 +48,8 @@ impl LraTheory {
 
         self.ints.push(is_int);
         self.reals.push(Self::zero());
-        self.lbs.push(Self::negative_inf());
-        self.ubs.push(Self::positive_inf());
+        self.lbs.push((None, Self::negative_inf()));
+        self.ubs.push((None, Self::positive_inf()));
         self.t_watches.push(HashSet::new());
 
         var
@@ -72,18 +72,18 @@ impl LraTheory {
     }
 
     pub(super) fn lb(&self, var: usize) -> &InfRational {
-        self.lbs.get(var).expect("variable index out of bounds")
+        &self.lbs.get(var).expect("variable index out of bounds").1
     }
 
     pub(super) fn ub(&self, var: usize) -> &InfRational {
-        self.ubs.get(var).expect("variable index out of bounds")
+        &self.ubs.get(var).expect("variable index out of bounds").1
     }
 
     pub(super) fn is_int(&self, var: usize) -> bool {
         *self.ints.get(var).expect("variable index out of bounds")
     }
 
-    pub(super) fn set_lb(&mut self, _lit: Lit, var: usize, new_lb: InfRational) -> bool {
+    pub(super) fn set_lb(&mut self, lit: Lit, var: usize, new_lb: InfRational) -> bool {
         assert!(var < self.reals.len(), "variable index out of bounds: {var}");
 
         if &new_lb <= self.lb(var) {
@@ -94,9 +94,12 @@ impl LraTheory {
             return false;
         }
 
-        self.bound_trail.push(BoundUpdate::LowerBound { var, val: self.lbs[var].clone() });
+        let (Some(c_lit), val) = self.lbs[var].clone() else {
+            panic!("lower bound should have a guard literal");
+        };
+        self.bound_trail.push(BoundUpdate::LowerBound { lit: c_lit, var, val });
 
-        self.lbs[var] = new_lb.clone();
+        self.lbs[var] = (Some(lit), new_lb.clone());
 
         if self.value(var) < &new_lb && !self.is_basic(var) {
             self.update(var, new_lb);
@@ -105,7 +108,7 @@ impl LraTheory {
         true
     }
 
-    pub(super) fn set_ub(&mut self, _lit: Lit, var: usize, new_ub: InfRational) -> bool {
+    pub(super) fn set_ub(&mut self, lit: Lit, var: usize, new_ub: InfRational) -> bool {
         assert!(var < self.reals.len(), "variable index out of bounds: {var}");
 
         if &new_ub >= self.ub(var) {
@@ -116,9 +119,12 @@ impl LraTheory {
             return false;
         }
 
-        self.bound_trail.push(BoundUpdate::UpperBound { var, val: self.ubs[var].clone() });
+        let (Some(c_lit), val) = self.ubs[var].clone() else {
+            panic!("upper bound should have a guard literal");
+        };
+        self.bound_trail.push(BoundUpdate::UpperBound { lit: c_lit, var, val });
 
-        self.ubs[var] = new_ub.clone();
+        self.ubs[var] = (Some(lit), new_ub.clone());
 
         if self.value(var) > &new_ub && !self.is_basic(var) {
             self.update(var, new_ub);
@@ -152,6 +158,60 @@ impl LraTheory {
         }
 
         self.reals[var] = new_value;
+    }
+
+    pub fn check(&mut self) -> Result<(), Vec<Lit>> {
+        loop {
+            // we search for a basic variable whose value is not within its bounds..
+            let var = self.tableau.iter().find_map(|(&var, _)| {
+                if self.value(var) < self.lb(var) {
+                    Some((var, self.lb(var).clone()))
+                } else if self.value(var) > self.ub(var) {
+                    Some((var, self.ub(var).clone()))
+                } else {
+                    None
+                }
+            });
+            if let Some((leaving, val)) = var {
+                // .. if we find one, we try to pivot it with a non-basic variable that can take it back within bounds
+                if self.value(leaving) < &val {
+                    let entering = self.tableau[&leaving].iter().find_map(|(&v, coeff)| if coeff.is_positive() && self.value(v) < self.ub(v) || coeff.is_negative() && self.value(v) > self.lb(v) { Some(v) } else { None });
+                    if let Some(entering) = entering {
+                        self.pivot_and_update(entering, leaving, val.clone());
+                    } else {
+                        let mut conflict = Vec::new();
+                        for (vr, vl) in &self.tableau[&leaving] {
+                            if vl.is_positive() {
+                                conflict.push(self.ubs[*vr].0.expect("upper bound should have a guard literal"));
+                            } else if vl.is_negative() {
+                                conflict.push(self.lbs[*vr].0.expect("lower bound should have a guard literal"));
+                            }
+                        }
+                        conflict.push(self.lbs[leaving].0.expect("lower bound should have a guard literal"));
+                        return Err(conflict);
+                    }
+                }
+                if self.value(leaving) > &val {
+                    let entering = self.tableau[&leaving].iter().find_map(|(&v, coeff)| if coeff.is_positive() && self.value(v) > self.lb(v) || coeff.is_negative() && self.value(v) < self.ub(v) { Some(v) } else { None });
+                    if let Some(entering) = entering {
+                        self.pivot_and_update(entering, leaving, val);
+                    } else {
+                        let mut conflict = Vec::new();
+                        for (vr, vl) in &self.tableau[&leaving] {
+                            if vl.is_positive() {
+                                conflict.push(self.lbs[*vr].0.expect("lower bound should have a guard literal"));
+                            } else if vl.is_negative() {
+                                conflict.push(self.ubs[*vr].0.expect("upper bound should have a guard literal"));
+                            }
+                        }
+                        conflict.push(self.ubs[leaving].0.expect("upper bound should have a guard literal"));
+                        return Err(conflict);
+                    }
+                }
+            } else {
+                return Ok(()); // all basic variables are within bounds, we are done
+            }
+        }
     }
 
     fn pivot(&mut self, entering: usize, leaving: usize) {
@@ -273,11 +333,11 @@ impl LraTheory {
             let update = self.bound_trail.pop().expect("trail should contain an update");
 
             match update {
-                BoundUpdate::LowerBound { var, val } => {
-                    self.lbs[var] = val;
+                BoundUpdate::LowerBound { lit, var, val } => {
+                    self.lbs[var] = (Some(lit), val);
                 }
-                BoundUpdate::UpperBound { var, val } => {
-                    self.ubs[var] = val;
+                BoundUpdate::UpperBound { lit, var, val } => {
+                    self.ubs[var] = (Some(lit), val);
                 }
             }
         }
@@ -287,6 +347,6 @@ impl LraTheory {
 }
 
 enum BoundUpdate {
-    LowerBound { var: usize, val: InfRational },
-    UpperBound { var: usize, val: InfRational },
+    LowerBound { lit: Lit, var: usize, val: InfRational },
+    UpperBound { lit: Lit, var: usize, val: InfRational },
 }
