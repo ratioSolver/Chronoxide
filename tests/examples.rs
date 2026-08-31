49b6fe1ea0327c43429dcc77bbae8dae582b5549
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-08-31 14:18:16 +0200

    Add tests for the Solver with example RDDL files
    - Introduced a macro `test_chronoxide` to streamline the creation of tests for the Solver.
    - Added three example RDDL files:
      - `example_00.rddl`: Demonstrates basic numeric constraints.
      - `example_01.rddl`: Demonstrates basic propositional constraints.
      - `example_02.rddl`: Introduces an inconsistent problem to test error handling.

---
+++ tests/examples.rs
@@ -0,0 +1,44 @@
+use chronoxide::Solver;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+macro_rules! test_chronoxide {
+    ($name:ident, ok, [ $($path:expr),+ ]) => {
+        #[tokio::test]
+        async fn $name() {
+            let slv = Solver::new();
+
+            $(
+                let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+                p.push($path);
+                assert!(slv.read(read_to_string(&p).unwrap_or_else(|_| panic!("Failed to read file: {}", $path))).await.is_ok(), "Failed to read RiDDle script from file: {}", $path);
+            )+
+
+            assert!(slv.solve().await.is_ok(), "Solver failed for test: {}", stringify!($name));
+        }
+    };
+
+    ($name:ident, err, [ $($path:expr),+ ]) => {
+        #[tokio::test]
+        async fn $name() {
+            let slv = Solver::new();
+            let mut read_failed = false;
+
+            $(
+                let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+                p.push($path);
+                if slv.read(read_to_string(&p).unwrap_or_else(|_| panic!("Failed to read file: {}", $path))).await.is_err() {
+                    read_failed = true;
+                }
+            )+
+
+            if !read_failed {
+                assert!(slv.solve().await.is_err(), "Solver unexpectedly succeeded for test: {}", stringify!($name));
+            }
+        }
+    };
+}
+
+test_chronoxide!(test_core_00, ok, ["tests/examples/core/example_00.rddl"]);
+test_chronoxide!(test_core_01, ok, ["tests/examples/core/example_01.rddl"]);
+test_chronoxide!(test_core_02, err, ["tests/examples/core/example_02.rddl"]);
